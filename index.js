10b51f9159bbb5e28eb8502acf3e7feecdbed08f
wangzhiwei35 <wangzhiwei11@jd.com>
2018-09-18 09:14:33 +0800

    support windows

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const path = require('path');
 const fs = require('fs');
 
 function isRoot(dir){
-	if(process.platform === 'win32'){
-		return /[a-zA-Z]:\\/.test(dir);
-	}else{
-		return dir !== '/';
-	}
+  if(process.platform === 'win32'){
+    return /[a-zA-Z]:\\/.test(dir);
+  }else{
+	return dir !== '/';
+  }
 }
 
 module.exports = function(dir, filename, retDir = false) {
