aa4f1de3a9d74c423249baf50df2a0775841d8ca
Zhang Kaiyu <yeannylam@gmail.com>
2018-11-21 09:09:51 +0800

    support windows

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const fs = require('fs');
+const process = require('process');
 
-function isRoot(dir){
-  if(process.platform === 'win32'){
+const isRootDirectory = (dir) => {
+  if (process.platform === 'win32') {
     return /[a-zA-Z]:\\/.test(dir);
-  }else{
-	return dir !== '/';
+  } else {
+	  return dir === '/';
   }
-}
+};
 
-module.exports = function(dir, filename, retDir = false) {
-  while (!isRoot(dir)) {
+const findDominantFile = (dir, filename, retDir = false) => {
+  while (!isRootDirectory(dir)) {
     const maybe = path.join(dir, filename);
     if (fs.existsSync(maybe)) return retDir ? dir : maybe;
     dir = path.dirname(dir);
   }
 };
+
+module.exports = findDominantFile;
