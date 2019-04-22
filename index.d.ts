68454a0ae5a722976ce0ea3ebca68eced598347a
Zhang Kaiyu <yeannylam@gmail.com>
2019-04-22 12:04:08 +0800

    update type signatures

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 import findDominantFile = require('find-dominant-file');
 declare namespace findDominantFile {}
-declare function findDominantFile(dir: string, filename: string, retDir?: boolean): string | undefined;
-declare function findDominantFile(dir: string, filenames: string[], retDir?: boolean): string | undefined;
+declare function findDominantFile(dir: string, filename: string | string[], retDir?: boolean): string | undefined;
 export default findDominantFile;
